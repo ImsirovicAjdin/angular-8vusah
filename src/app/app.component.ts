ca92b66b48b66c897b783b00cad6204a2ccc7a96
ImsirovicAjdin <ajdika@live.com>
2019-07-20 07:11:27 +0200

    One-way binding from class to template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,10 +13,21 @@ Revising some known stuff from:
 Angular: Getting Started (Deborah Kurata)
 
 Property Binding:
+###############################################
 <img [src]='product.imageUrl'>
       (1)         (2)
 
-(1) - element property
+(1) - element property 
 (2) - template expression
+
+[] - binding target
+'' - binding source
+
+So we're binding from the Component Class property ('') to the element property ([]). It's a one-way binding from the *.ts file to the *.html file.
+
+<img src={{ product.imageUrl }}
+<img src='http://openclipart.org/{{product.imageUrl}}'>
+
+
 
 */
